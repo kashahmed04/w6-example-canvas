582c8c37ab47e81be97273f7b0aa9a063bb906c6
Kashaf Ahmed <kka7705@g.rit.edu>
2024-02-19 13:26:43 -0500

    Updated

--- src/main.ts
+++ src/main.ts
@@ -37,3 +37,17 @@ alphaButton.addEventListener('click', () => {
   context.fillStyle = 'rgba(255, 255, 255, 0.1)'
   context.fillRect(300, 300, 300, 300)
 })
+
+// EXAMPLE 6:
+// we have 3 buttons for clear rectangle and it empties out portion of the canvas or if we dont want it to be transparent we draw
+// white over it for the second button and the third button fades to almost white but it wont quite get there**
+
+// we have our image we load it and draw it and we have buttons setup and we have click events setup on each of them and clearrect clears it out
+// with black with 0 opactity for the first button (its not black though)**
+// the second button makes it white and the third button makes it transparent each time we click**
+
+// we have image and its 1000 pixels wide and 60 ish tall and we are going to have the starting cooridnates to start drawing rectangle and
+// we have width and height for how we want to draw it based on the starting cooridnates and the other cooridnates is where we start drawing the
+// image and how big it is (8 numbers source,x,y (starting),width,height (to start),destination x,y,and destintion width and height)
+
+// */
