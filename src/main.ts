971c56375911d92309fc0d3e3a077dc0e865bc7b
Kashaf Ahmed <kka7705@g.rit.edu>
2024-02-19 17:53:05 -0500

    Update

--- src/main.ts
+++ src/main.ts
@@ -13,20 +13,31 @@ canvas.height = SIZE
 // like the AudioContext, we have a CanvasRenderingContext2D
 const context = canvas.getContext('2d') as CanvasRenderingContext2D
 
-// solid white fill of the canvas since context is the canvas renderer responsible for showing everything on the screen**
-//we dont use canvas here because it makes the actual canvas itself and does not rednder our drawings**
+// solid white fill of the canvas since context is the canvas renderer responsible for showing everything on the screen
+//(yes)
+//we dont use canvas here because it makes the actual canvas itself and does not rednder our drawings (yes)
 context.fillStyle = 'rgb(255, 255, 255)'
-// draw the white background for the canvas**
+// draw the white background for the canvas (yes)
 context.fillRect(0, 0, SIZE, SIZE)
 
-//how does it know to make background purple**
+//how does it know to make background purple (html in CSS)
 
 // Yes, I absolutely could have written these with a loop.
 // But I didn't, to be explicit about what values are being passed to the draw methods.
 // (in other words, making it easier to follow the example without having to math out the values)
 
-//go over all types of strokes and what do the width strokes mean (on the slides as well about the width of the strokes)**
-//which stroke widths are usually the best to use or commonly used**
+//when we are specifying coorindates on the canvas we talk about pixel boundaries (drawing rectangle in first image
+//because the red line is on the black line and its easy to show pixels to be filled (whole pixels))
+
+//in the middle image we are drawing an image and the line width is 1 pixel so the green says if we have width of 1 pixel that
+//means the dark blue region is what the line is but we cant have half a pixel be colred and not be colored and the
+//lightblue becomes gray and it makes the line look fuzzier than it should be
+
+//to fix the second image, in the third image if we have a line width of 1 we should have our coordinates in half
+//pixels with the x-axis so when we center the line it falls within the pixels and theres no gray area for blurriness
+
+//for odd numbers if we divide them in half we dont get a whole number of pixels which contribute to bluriness
+//for even numbers it wont do that and we have whole numbers so we can have clearer lines (for the line width)
 
 // The first series has odd-width strokes on whole-number pixel coordinates
 context.strokeStyle = 'black'
@@ -182,12 +193,9 @@ context.stroke()
 
 // CANVAS LINES:
 
-// (0,0) is the top left of the canvas and when we draw a line if we go from (3,1) to (3,5) it will go by half of a pixel
-// so we say go 2 or**
-
 // we set the stoke style (color), the linewidth (thickness of line), beginpath()(we are about to start drawing a path then we can do
-// series of moveto() and lineto() afterwards)** moveto()(moves position of pen without touching paper) lineto()(put pen down to start drawing)**
-// the moveto and lineto dont draw automatically so we do a stroke afterwards at the very end to actually draw a shape**
+// series of moveto() and lineto() afterwards) moveto()(moves position of pen without touching paper) lineto()(put pen down to start drawing)
+// the moveto and lineto dont draw automatically so we do a stroke afterwards at the very end to actually draw a shape
 
 // context.strokeStyle = 'black'
 // context.lineWidth = 1
@@ -195,10 +203,3 @@ context.stroke()
 // context.moveTo(20, 20)
 // context.lineTo(20, 70)
 // context.stroke()
-
-// EXAMPLE lines:
-
-// for first row of lines the first line uses an odd number line width on whole number pixels**
-// the second row of lines line makes the offset with half the linewidth**
-// for the third row of lines we have even width lines because its half pixel location**
-// for the fourth row lines**
