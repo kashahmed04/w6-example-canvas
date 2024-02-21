89270058a4dc1a6b933a9a92a88ebb37350eda7e
Kashaf Ahmed <kka7705@g.rit.edu>
2024-02-21 10:08:57 -0500

    Updated

--- src/main.ts
+++ src/main.ts
@@ -47,3 +47,18 @@ canvas.addEventListener('mousemove', (event: MouseEvent) => {
   mouse.x = event.offsetX
   mouse.y = event.offsetY
 })
+
+/*
+dot that follows the mouse and to have it so there is no marks as we move the mouse we have to clear the canvas as we move the mouse and
+if we dont clear the canvas the cusor draws as we move the mouse**
+hold x and y positin and have mouse be at x and y and there is an event listender for the mousemove and every time the mouse
+moves ontop of canvas element we save the offset of our mouse**
+
+clipboard is page and green page is the canvas and (0,0) is the beginning of the canvas and the x offset is from left side to x point
+and y is from top of screen to y offset of mouse**
+
+we have renderer in that is like a loop for each refresh of the canvas and each time the canvas refrehes it**
+
+in the css we dont have the arrow but we change it to the circle within the canvas but if we go outside the canvas
+then it turns back to cursoe**
+ */
